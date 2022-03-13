846dcad3a381564fd18a3ff92ba0fe5d80644c9e
thowhidulislam <thowhid.ndc14@gmail.com>
2022-03-13 12:58:19 +0600

    card element created and added to div

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }
+]
+cardArray.sort(() => 0.5 - Math.random())
+
+// creating card and appending to div 'grid'
+const gridDisplay = document.querySelector('#grid')
+const createBoard = () => {
+    cardArray.forEach(cardElement => {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', cardArray.indexOf(cardElement))
+        gridDisplay.appendChild(card)
+    })
+}
+createBoard()
